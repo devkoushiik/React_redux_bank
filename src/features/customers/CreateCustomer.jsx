ff3d9f1373fd5971f38f8663eaa1446a50d7c0a1
Koushik Ahmed <koushikxahmed@gmail.com>
2023-08-25 00:05:13 +0600

    User system implemented.

--- src/features/customers/CreateCustomer.jsx
+++ src/features/customers/CreateCustomer.jsx
@@ -9,6 +9,7 @@ function Customer() {
   const dispatch = useDispatch();
 
   function handleClick() {
+    if (!fullName || !nationalId) return;
     dispatch(createCustomer(fullName, nationalId));
   }
 
